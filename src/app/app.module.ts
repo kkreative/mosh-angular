cb10d9a531f6ab4858f5678e0712178258555c36
Kevin Townsend <kkre8iv@outlook.com>
2017-10-09 10:20:40 -0400

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CoursesComponent } from './courses.component';
+import { CourseComponent } from './course/course.component';
+import { CoursesService } from './courses.service';
+import { AuthorsComponent } from './authors/authors.component';
+import { AuthorsService } from './authors.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CoursesComponent,
+    CourseComponent,
+    AuthorsComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    CoursesService,
+    AuthorsService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/authors.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class AuthorsService {
+
+  constructor() {
+
+   }
+
+   getAuthors() {
+    return ['author1', 'author2', 'author3'];
+   }
+
+}
